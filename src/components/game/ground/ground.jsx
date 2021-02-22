737b4a58d2896e15b02c1722517a181ef8fa06c4
Anatoly Yakyshenko <anatoly.yakyshenko@gmail.com>
2021-02-22 16:26:55 +0300

    feat: add animation ground

--- src/components/game/ground/ground.jsx
+++ src/components/game/ground/ground.jsx
@@ -1,9 +1,12 @@
 import styles from "./ground.module.css";
 
 const Ground = (props) => {
-    return (
-        <div className={styles.item}></div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
+};
 
 export default Ground;
